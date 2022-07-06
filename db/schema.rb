2e060469605fe618e70cc0f8fd80dddc7f953e18
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-06 12:45:55 +0900

    Userテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_143309) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_033629) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.boolean "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "uid"
+    t.string "cid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
