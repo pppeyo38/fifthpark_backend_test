a39319cc461cde13eff3aeea9ab11c34e67cc953
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-08 23:02:53 +0900

    postテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_06_033629) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.boolean "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "uid"
