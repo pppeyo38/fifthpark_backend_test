981c5d2bfc7b962b692228243c6bc9052f79bf96
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-08 23:52:52 +0900

    ポストモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_06_033629) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_143700) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "uid"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_06_033629) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
