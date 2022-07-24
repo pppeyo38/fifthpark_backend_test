0815f529c4d728670059ed2e76a8320948da8e26
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-24 14:19:01 +0900

    remove users_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::API
 
     def current_user
         authenticate_with_http_token do |id_token, _options|
-            uid = FirebaseAuth.verify_id_token(id_token)[:uid]
-            @current_user ||= User.find_by(firebase_id: uid)
-        rescue => e
+            uid = FirebaseAuth.verify_id_token(id_token)["uid"]
+            @current_user ||= User.find_by(firebase_uid: uid)
+        rescue StandardError => e
             Rails.logger.error(e.message)
             nil
         end
