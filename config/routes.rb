5398a95d1df5150c351d11ca97997b64f4f36231
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-24 13:54:01 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
-  # resources :users
-  get 'profile', to: 'users#profile'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/profile', to: 'account#show'
+  put '/profile', to: 'account#update'
+
+  post '/signup', to: 'auth#signup'
 end
